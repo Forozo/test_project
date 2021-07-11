6c5367e2e92a2e2d57cce51c45d999a9368a207a
Saeed <saeedamiri1997@gmail.com>
2021-07-11 17:59:12 +0430

    Auth commit

--- routes/api.php
+++ routes/api.php
@@ -13,16 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::prefix('/book_v3')->group(function () {
-    Route::post('upload', [\App\Http\Controllers\Controller::class, 'upload']);
-    Route::get('download', [\App\Http\Controllers\Controller::class, 'download']);
-
-});
-
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::prefix('/login')->group(function (){
     Route::post('register',[\App\Http\Controllers\AuthController::class,'register']);
